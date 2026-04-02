c345d0b033c5e253b589b5360a0b2b62b1085740
sawyer100 <leonsawyr888@gmail.com>
2026-04-02 16:32:06 -0700

    dev notes

--- src/game/main.js
+++ src/game/main.js
@@ -1,35 +1,32 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
-import { AUTO, Game } from 'phaser';
+import { Boot } from "./scenes/Boot";
+import { Game as MainGame } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import { Preloader } from "./scenes/Preloader";
+import { AUTO, Game } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
-    type: AUTO,
-    width: 1280,
-    height: 720,
-    parent: 'game-container',
-    backgroundColor: '#000000', 
-    scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: AUTO,
+  // we're using RESIZE scale mode so, this is jsut the efault width and height, itll auto-change when the game starts dont worry abt this
+  width: 1280,
+  height: 720,
+  parent: "game-container",
+  backgroundColor: "#000000",
+  scale: {
+    //automatically size to the entire screen
+    mode: Phaser.Scale.RESIZE,
+    //centers hegith and width
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+
+  // to make a scene playable, import the scene's .js file and put it in here
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 const StartGame = (parent) => {
-
-    return new Game({ ...config, parent });
-
-}
+  return new Game({ ...config, parent });
+};
 
 export default StartGame;
--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,10 +1,13 @@
 import { Scene } from "phaser";
 
+// create main menu as a scene
 export class MainMenu extends Scene {
+  // call this the MainMenu so phaser knows how to identify it
   constructor() {
     super("MainMenu");
   }
 
+  //create method runs when the scene starts and all needed assets for it are already loaded
   create() {
     this.background = this.add.image(0, 0, "background").setOrigin(0, 0);
 
@@ -21,15 +24,20 @@ export class MainMenu extends Scene {
       })
       .setOrigin(0.5);
 
+    // this method places / resizes things based on ur screen size
     this.layout();
 
+    // checks if screen gets resized and updates with layout()
     this.scale.on("resize", this.layout, this);
 
+    // remove the listner for screen resizing after you exit the scene (cleanup)
     this.events.once("shutdown", () => {
       this.scale.off("resize", this.layout, this);
     });
 
+    //starts the game when the player clicks
     this.input.once("pointerdown", () => {
+      //switches to the scene Game
       this.scene.start("Game");
     });
   }
